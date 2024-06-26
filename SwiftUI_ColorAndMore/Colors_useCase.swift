3b21ad0413728c0f76df88a89d92b6943cb81f05
dev766 <akash>
2024-06-27 02:00:22 +0530

    test

---
+++ SwiftUI_ColorAndMore/Colors_useCase.swift
@@ -0,0 +1,18 @@
+//
+//  Colors_useCase.swift
+//  SwiftUI_ColorAndMore
+//
+//  Created by Apple on 27/06/24.
+//
+
+import SwiftUI
+
+struct Colors_useCase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Colors_useCase()
+}
