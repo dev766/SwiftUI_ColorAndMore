90819441866f52380903ff4d731de8ffbace3175
dev766 <akash>
2024-06-27 01:22:05 +0530

    Initial Commit

---
+++ SwiftUI_ColorAndMore/SwiftUI_ColorAndMoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ColorAndMoreApp.swift
+//  SwiftUI_ColorAndMore
+//
+//  Created by Apple on 27/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ColorAndMoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
